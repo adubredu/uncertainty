8dd7033d33efc9c5359c8fcf0de1340b74eb806d
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2020-06-10 11:15:16 -0400

    done training new detector

--- detector/train_detector.py
+++ detector/train_detector.py
@@ -1,11 +1,21 @@
 from detecto import core, utils, visualize
+from torchvision import transforms
 
-dataset = core.Dataset('labels/')
+augmentations = transforms.Compose([
+    transforms.ToPILImage(),
+    transforms.RandomHorizontalFlip(0.5),
+    transforms.ColorJitter(saturation=0.5),
+    transforms.ToTensor(),
+    utils.normalize_transform(),
+])
+
+
+dataset = core.Dataset('labels/',transform=augmentations)
 model = core.Model(['ambrosia','apple','banana','bottle','cereal','coke',\
 				'lipton','lysol','milk','nutella','orange','oreo','pepsi'])
 model.fit(dataset)
 print('Done training')
-image = utils.read_image('labels/cereal.png')
+image = utils.read_image('labels/bottle.png')
 predictions = model.predict(image)
 
 labels,boxes,scores = predictions
@@ -13,3 +23,17 @@ print(labels)
 print(scores)
 
 model.save('grocery_detector.pth')
+
+# from detecto import utils, visualize, core
+
+# model = core.Model.load('grocery_detector.pth', \
+# 		['ambrosia','apple','banana','bottle','cereal','coke',\
+# 				'lipton','lysol','milk','nutella','orange','oreo','pepsi'])
+
+# image = utils.read_image('labels/bottle.png')
+
+# labels, boxes, scores = model.predict(image)
+# predictions = model.predict_top(image)
+
+
+# print(labels[0])
