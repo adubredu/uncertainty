4a65db8e562f0b778a56eca452b6503458ca62e9
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2020-06-10 18:39:34 -0400

    confident bounding box and name implemented

--- simulation/models/view_model.py
+++ simulation/models/view_model.py
@@ -20,13 +20,13 @@ planeId = p.loadURDF("plane.urdf")
 tableid = p.loadURDF('table/table.urdf',[0,0,0],p.getQuaternionFromEuler([0,0,0]))
 
 p.setAdditionalSearchPath('/home/developer/uncertainty/simulation/models')
-# bottle = p.loadURDF('bottle/bottle.urdf',[0,0,0.65],p.getQuaternionFromEuler([1.57,0,0]))
-# apple = p.loadURDF('apple/apple.urdf',[0.5,0,0.65],p.getQuaternionFromEuler([3.14,0,0]))
-coke = p.loadURDF('coke/coke.urdf',[1.0,0,0.65],p.getQuaternionFromEuler([1.57,0,0]))
-orange = p.loadURDF('orange/orange.urdf',[0,0,0.65],p.getQuaternionFromEuler([0,0,0]))
+bottle = p.loadURDF('bottle/bottle.urdf',[0,0,0.65],p.getQuaternionFromEuler([1.57,0,0]))
+apple = p.loadURDF('apple/apple.urdf',[0.25,0,0.65],p.getQuaternionFromEuler([3.14,0,0]))
+# coke = p.loadURDF('coke/coke.urdf',[0.25,0.3,0.65],p.getQuaternionFromEuler([1.57,0,0]))
+orange = p.loadURDF('orange/orange.urdf',[1.5,0,0.65],p.getQuaternionFromEuler([0,0,0]))
 pepsi = p.loadURDF('pepsi/pepsi.urdf',[2,0,0.66],p.getQuaternionFromEuler([1.57,0,0]))
 cereal = p.loadURDF('cereal/cereal.urdf',[2.5,0,0.65],p.getQuaternionFromEuler([1.57,0,0]))
-lysol = p.loadURDF('lysol/lysol.urdf',[3,0,0.65],p.getQuaternionFromEuler([0,0,0]))
+lysol = p.loadURDF('lysol/lysol.urdf',[3,0,0.75],p.getQuaternionFromEuler([0,0,0]))
 lipton = p.loadURDF('lipton/lipton.urdf',[3.5,0,0.65],p.getQuaternionFromEuler([0,0,0]))
 nutella = p.loadURDF('nutella/nutella.urdf',[4,0,0.65],p.getQuaternionFromEuler([0,0,0]))
 ambrosia = p.loadURDF('ambrosia/ambrosia.urdf',[4.5,0,0.65],p.getQuaternionFromEuler([0,0,0]))
@@ -35,6 +35,25 @@ milk = p.loadURDF('milk/milk.urdf',[5.5,0,0.65],p.getQuaternionFromEuler([0,0,0]
 banana = p.loadURDF('banana/banana.urdf',[6,0,0.65],p.getQuaternionFromEuler([0,0,0]))
 
 p.setRealTimeSimulation(1)
+
+def normalize_scene_weights(scene):
+	norm_scene = {}
+
+	for item in scene:
+		names=[]; weights=[];coord=[]
+		norm_scene[item]=[]
+		for name,wt,cd in scene[item]:
+			names.append(name)
+			weights.append(wt)
+			coord.append([int(c) for c in cd])
+
+		summ = np.sum(weights)
+		norm_wt = weights/summ
+		for name, wt, cd in zip(names, norm_wt, coord):
+			norm_scene[item].append((name,wt,cd))
+
+	return norm_scene
+
 
 
 while 1:
@@ -62,8 +81,8 @@ while 1:
 	
 	rgbImg = Image.fromarray(rgbImg).convert('RGB')
 	predictions = model.predict(rgbImg)
-	# camera_view = cv2.cvtColor(np.array(rgbImg), cv2.COLOR_RGB2BGR)
-	camera_view = np.array(rgbImg)
+	camera_view = cv2.cvtColor(np.array(rgbImg), cv2.COLOR_RGB2BGR)
+	# camera_view = np.array(rgbImg)
 
 	labels, boxes, scores = predictions
 	boxes = boxes.numpy()
@@ -93,7 +112,7 @@ while 1:
 		mean = np.sqrt((box['coordinates'][0]-box['coordinates'][2])**2 +\
 						(box['coordinates'][1]-box['coordinates'][3])**2)
 		for key in clusters:
-			if np.abs(key - mean) < 30:
+			if np.abs(key - mean) < 10:
 				clusters[key].append(box)
 				fit = True
 				break
@@ -105,25 +124,40 @@ while 1:
 		weights = [box['confidence'] for box in clusters[key]]
 		maxind = np.argmax(weights)
 		maxweightedbox = clusters[key][maxind]
-		scene[maxweightedbox['name']] = [(box['name'], box['confidence']) for box in clusters[key]]
+		scene[maxweightedbox['name']] = [(box['name'], box['confidence'], \
+						box['coordinates']) for box in clusters[key]]
 
-	print(scene)
+	nscene = normalize_scene_weights(scene)
+	# print(nscene)
 
-	print('**********************************')
+	# print('**********************************')
+
+	for item in nscene:
+		for nm, cf, cd in nscene[item]:
+			if nm == item:
+				color = (np.random.randint(255),\
+				 		np.random.randint(255),\
+				 		np.random.randint(255))
+				camera_view = cv2.rectangle(camera_view, (cd[0],
+				 cd[1]), (cd[2], cd[3]),color , 1)
+				cv2.putText(camera_view, nm+':'+str(round(cf,2)), (cd[0],cd[1]-10),\
+					cv2.FONT_HERSHEY_SIMPLEX, 0.5, color,2)
 
 
 
-	for item in observed:
-		camera_view = cv2.rectangle(camera_view, (observed[item]['coordinates'][0],
-				 observed[item]['coordinates'][1]), (observed[item]['coordinates'][2], 
-				 observed[item]['coordinates'][3]), observed[item]['color'], 1)
+
+
+	# for item in observed:
+	# 	camera_view = cv2.rectangle(camera_view, (observed[item]['coordinates'][0],
+	# 			 observed[item]['coordinates'][1]), (observed[item]['coordinates'][2], 
+	# 			 observed[item]['coordinates'][3]), observed[item]['color'], 1)
 
 
 
 	# print(observed)
-	print('\n\n')
+	# print('\n\n')
 	# cv2.rectangle(camera_view, (1,1), (100,100),(0,255,0),1)
-	cv2.imshow('ss', camera_view)
+	cv2.imshow('Grocery Item detection', camera_view)
 	if cv2.waitKey(1) & 0xFF == ord('q'):
 	    break
 	p.stepSimulation()
