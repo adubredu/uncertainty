338272381b0e941090862eabbb31c2be71395976
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2020-06-11 11:36:01 -0400

    full box arrangement implemented

--- simulation/belief_3D.py
+++ simulation/belief_3D.py
@@ -14,6 +14,57 @@ print(pybullet_data.getDataPath())
 p.setGravity(0,0,0)
 planeId = p.loadURDF("plane.urdf") 
 
+class Box:
+    def __init__(self, bottom_capacity):
+        self.cpty = bottom_capacity
+        self.index = 0
+        self.old_index = 0
+        self.lx = 260 
+        self.ly = 290
+        self.ys = [-0.1, 0, 0.1]
+        self.xs = [-0.6, -0.5, -0.4]
+        self.z = 0.7
+        #i is per row, j is per column
+        self.occupancy = [[0 for j in range(self.cpty)] for i in range(self.cpty)]
+        self.items_added = {}
+        self.to_resolve = False
+        self.num_items = 0
+        self.cascade = False
+
+    def add_item(self, item):
+        # self.items_added[item.name] = self.index%self.cpty
+        self.num_items+=1
+        xyind = (99,99)
+        for j in range(self.cpty):
+        	for i in range(self.cpty):
+	            if self.occupancy[i][j] == 0:
+	                xyind = (i,j)
+	                break
+        if xyind[0] != 99:
+            x = self.xs[xyind[0]]
+            y = self.ys[xyind[1]]
+            self.occupancy[xyind[0]][xyind[1]] = 1
+            self.items_added[item] = xyind
+        else:
+        	print('Box full')
+        	return (99,99,99)
+            # x = self.widths[self.index%self.cpty]
+            # if self.cascade: 
+            #     y = self.heights[self.index%self.cpty]- item.height                 
+            # else:
+            #     y = self.ly - item.height 
+
+            # self.heights[self.index%self.cpty] = y
+            # self.index +=1
+        return x,y,self.z
+
+    def remove_item(self, item):
+        if item.name in self.items_added:
+            index = self.items_added[item]
+            self.occupancy[index[0]][index[1]] = 0
+            self.items_added.pop(item)
+            self.num_items-=1
+
 
 
 class Grocery_item:
@@ -46,32 +97,54 @@ class Grocery_item:
 
 class Grocery_packing:
 	def __init__(self):
-		self.cube = Grocery_item(.5,0.,0.65, 0,0,0, "cube_small.urdf",0.07,0.07,0.07,'cube','light')
 		self.table = Grocery_item(0,0,0, 0,0,0, "table/table.urdf",1,1,1,'table','heavy')
 		self.lgripper = Grocery_item(0.2,0.0,1.5, 0,3.14,3.14, "gripper/wsg50_one_motor_gripper_left_finger.urdf",1,1,1,'lgripper','light')
 		self.rgripper = Grocery_item(0.23,0.0,1.5, 0,3.14,3.14, "gripper/wsg50_one_motor_gripper_right_finger.urdf",1,1,1,'rgripper','light')
 		p.setAdditionalSearchPath('/home/developer/uncertainty/simulation/models')
-
-		self.tray = Grocery_item(-.5,.0,0.62, 0,0,0, "container/container.urdf",1,1,1,'tray','heavy')
-
+		self.tray = Grocery_item(-.5,.0,0.62, 0,0,0, "container/container.urdf",0.3,0.3,0.3,'tray','heavy')
 		self.items = {
-						'cube':self.cube,
 						'table':self.table,
 						'lgripper':self.lgripper,
 						'rgripper':self.rgripper,
 						'tray': self.tray
 		}
+		self.init_clutter()
+
+		
 		self.delta = 0.01
 		self.fps = 60
 		self.scene_belief = None
-		perception = threading.Thread(target=self.start_perception,args=(1,))
-		perception.start()
+		# perception = threading.Thread(target=self.start_perception,args=(1,))
+		# perception.start()
 
 
 	def refresh_world(self):
 		for key in self.items:
 			self.items[key].update_object_position()
 		p.stepSimulation()
+
+
+	def init_clutter(self):
+		self.bottle = Grocery_item(.5,0.,0.65, 1.57,0,0, "bottle/bottle.urdf",0.07,0.07,0.07,'bottle','light')
+		self.coke = Grocery_item(.5,.1,.65, 0,0,0, 'coke/coke.urdf', 0.07,0.07,0.07,'coke','light')
+		self.nutella = Grocery_item(.6, 0., .65, 0,0,0, 'nutella/nutella.urdf', 0.07,0.07,0.07,'nutella','light')
+		self.orange = Grocery_item(.6,.1,.65,0,0,0, 'orange/orange.urdf', 0.07,0.07,0.07, 'orange','light')
+		self.pepsi = Grocery_item(.5, -.1, .65, 1.57, 0,0,'pepsi/pepsi.urdf',0.07,0.07,0.07, 'pepsi','light')
+		self.cereal = Grocery_item(.5, .2, .65, 1.57,0,0, 'cereal/cereal.urdf',0.07,0.07,0.07, 'cereal','light')
+		self.lysol = Grocery_item(.6, -.1, .65, 0,0,0, 'lysol/lysol.urdf', 0.07,0.07,0.07, 'lysol','light')
+		self.lipton = Grocery_item(.6, .2, .65, 0,0,0, 'lipton/lipton.urdf',0.07,0.07,0.07, 'lipton','light' )
+		self.apple = Grocery_item(.7, 0., .65, 3.14,0,0, 'apple/apple.urdf', 0.07,0.07,0.07, 'apple','light')
+
+		self.items['bottle'] = self.bottle
+		self.items['coke'] = self.coke
+		self.items['nutella'] = self.nutella
+		self.items['orange'] = self.orange
+		self.items['pepsi'] = self.pepsi 
+		self.items['cereal'] = self.cereal
+		self.items['lysol'] = self.lysol
+		self.items['lipton'] = self.lipton
+		self.items['apple'] = self.apple
+
 
 
 	def start_perception(self,x):
@@ -90,9 +163,10 @@ class Grocery_packing:
 				names=[]; weights=[];coord=[]
 				norm_scene[item]=[]
 				for name,wt,cd in scene[item]:
-					names.append(name)
-					weights.append(wt)
-					coord.append([int(c) for c in cd])
+					if cd[0] > 240:
+						names.append(name)
+						weights.append(wt)
+						coord.append([int(c) for c in cd])
 
 				summ = np.sum(weights)
 				norm_wt = weights/summ
@@ -175,7 +249,7 @@ class Grocery_packing:
 			self.scene_belief = normalize_scene_weights(scene)
 			for item in self.scene_belief:
 				for nm, cf, cd in self.scene_belief[item]:
-					if nm == item and cf > 0.5 and cd[0]>240:
+					if nm == item and cf > 0.5:
 						color = (np.random.randint(255),\
 						 		np.random.randint(255),\
 						 		np.random.randint(255))
@@ -209,6 +283,7 @@ class Grocery_packing:
 			else:
 				self.rgripper.x-=self.delta
 			time.sleep(1./self.fps)
+
 			self.refresh_world()
 
 		while math.fabs(self.lgripper.y - item.y)>self.delta or math.fabs(self.rgripper.y - item.y)>self.delta:
@@ -265,6 +340,7 @@ class Grocery_packing:
 				item.y-=self.delta
 			self.rgripper.y = self.lgripper.y
 			time.sleep(1./self.fps)
+
 			self.refresh_world()
 
 
@@ -361,8 +437,44 @@ for i in range(1):
 	# p.stepSimulation()
 	time.sleep(1./420)
 	g = Grocery_packing()
-	g.pick_up('cube')
-	g.put_in_box('cube',-0.6,0,0.7)
+	box = Box(3)
+	time.sleep(10)
+
+	g.pick_up('bottle')
+	i,j,k = box.add_item('bottle')
+	g.put_in_box('bottle',i,j,k)
+
+	g.pick_up('nutella')
+	i,j,k = box.add_item('nutella')
+	g.put_in_box('nutella',i,j,k)
+
+	g.pick_up('coke')
+	i,j,k = box.add_item('coke')
+	g.put_in_box('coke',i,j,k)
+
+	g.pick_up('cereal')
+	i,j,k = box.add_item('cereal')
+	g.put_in_box('cereal',i,j,k)
+
+	g.pick_up('lipton')
+	i,j,k = box.add_item('lipton')
+	g.put_in_box('lipton',i,j,k)
+
+	g.pick_up('orange')
+	i,j,k = box.add_item('orange')
+	g.put_in_box('orange',i,j,k)
+
+	g.pick_up('pepsi')
+	i,j,k = box.add_item('pepsi')
+	g.put_in_box('pepsi',i,j,k)
+
+	g.pick_up('apple')
+	i,j,k = box.add_item('apple')
+	g.put_in_box('apple',i,j,k)
+
+	g.pick_up('lysol')
+	i,j,k = box.add_item('lysol')
+	g.put_in_box('lysol',i,j,k)	
 	time.sleep(60)
 
 	# p.resetBasePositionAndOrientation(boxId, [h,0,1], cubeStartOrientation)
