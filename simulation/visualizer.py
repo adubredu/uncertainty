bfb49d397bc07264cb121bfd9424879dc478b1b8
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2020-06-14 22:16:39 -0400

    bare-bones of visualization complete

--- simulation/visualizer.py
+++ simulation/visualizer.py
@@ -1,9 +1,10 @@
 import rospy
 from std_msgs.msg import String, Bool
 import pygame
+import time
 
 pygame.init()
-pygame.display.set_caption("Grocery Packing visualizer")
+pygame.display.set_caption("Grocery Packing Visualizer")
 
 
 class  visualizer:
@@ -12,9 +13,9 @@ class  visualizer:
 		rospy.Subscriber('/plan', String, self.plan_callback)
 		rospy.Subscriber('/current_action', String, self.current_action_callback)
 		rospy.Subscriber('/stay_alive', Bool, self.alive_callback)
-		self.scene_belief = None
-		self.current_action = None
-		self.plan = None 
+		self.scene_belief = 'None'
+		self.current_action = 'None'
+		self.plan = 'None' 
 		self.stay_alive = True
 		self.clock = pygame.time.Clock()
 
@@ -23,49 +24,76 @@ class  visualizer:
 
 		self.fps = 60
 		self.win = pygame.display.set_mode((self.window_width,self.window_height))
+		self.win.fill((255,255,255))
 
-
-		self.object_coordinates = [(350,100), (400,100), (450,100), 
-								   (350,200), (400,200), (450,200), 
-								   (350,300), (400,300), (450,300), 
-								   (350,400), (400,400), (450,400), 
-								   (350,500), (400,500), (450,500), 
-								   (350,600), (400,600), (450,600)]
+		self.object_coordinates = [(50,100), (150,100), (250,100), 
+								   (50,200), (150,200), (250,200), 
+								   (50,300), (150,300), (250,300), 
+								   (50,400), (150,400), (250,400), 
+								   (50,500), (150,500), (250,500), 
+								   (50,600), (150,600), (250,600)]
 
 		rospy.spin()
 
 
 	def belief_callback(self, inputs):
 		self.scene_belief = inputs.data
+		self.refresh_window()
 
 
 	def plan_callback(self, inputs):
 		self.plan = inputs.data
+		self.refresh_window()
+
 
 
 	def current_action_callback(self, inputs):
 		self.current_action = inputs.data
+		self.refresh_window()
 
 
 	def alive_callback(self, inputs):
 		self.stay_alive = inputs.data
+		self.refresh_window()
 
 
-	def display_text(self,textcontent,x,y):
-		font = pygame.font.Font('freesansbold.ttf',12)
-		text = font.render(textcontent, True, (0,0,0))
+	def display_text(self,textcontent,x,y,color,font):
+		font = pygame.font.Font('freesansbold.ttf',font)
+		text = font.render(textcontent, True, color)
 		self.win.blit(text, (x,y))
 
 
-	def run(self):
-		objects = ['pepsi', 'nutella','coke','lipton',
-					'bleach', 'ambrosia', 'banana', 'cereal', 'lysol',
-					'milk', 'oreo', 'tangerine']
-		for i in range(10000):
-			for name, (x,y) in zip(objects, self.object_coordinates):
-				self.win.blit(pygame.image.load('assets/'+name+'.jpg'), (x,y))
-			pygame.display.update()
-			self.clock.tick(self.fps)
+	def refresh_window(self):
+		# objects = ['pepsi-0.8', 'nutella-0.8','coke-0.8','lipton-0.8',
+		# 			'bleach-0.8', 'ambrosia-0.8', 'banana-0.8', 'cereal-0.8', 
+		# 			'lysol-0.8','milk-0.8', 'oreo-0.8', 'tangerine-0.8']
+		self.win.fill((255,255,255))
+		self.display_text('Belief Space Confidence Probabilities', 20, 30, (0,0,255),17)
+		pygame.draw.line(self.win, (0,0,0), (350,0), (350,700),2)
+		self.display_text('Current Action: ', 360, 30, (0,0,255),17)
+		self.display_text(self.current_action, 360, 60, (0,0,0), 15)
+		pygame.draw.line(self.win, (0,0,0), (350,90), (800,90),2)
+		self.display_text('Current Plan: ', 360, 120, (0,0,255),17)
+		y = 150
+		plan = self.plan.split('_')[:-1]
+		for p in plan:
+			self.display_text(p, 360, y, (0,0,0), 15)
+			y+=20
+
+
+
+
+
+
+		objects = self.scene_belief.split('_')[:-1]
+
+		for name, (x,y) in zip(objects, self.object_coordinates):
+			nc = name.split('-')
+			nm = nc[0]; cf=nc[1]
+			self.win.blit(pygame.image.load('assets/'+nm+'.jpg'), (x,y))
+			self.display_text(cf, x, y-16, (0,0,0),14)
+		pygame.display.update()
+		self.clock.tick(self.fps)
 
 
 
@@ -73,4 +101,3 @@ class  visualizer:
 if __name__ == '__main__':
 	rospy.init_node('visualizer')		
 	v = visualizer()
-	v.run()
