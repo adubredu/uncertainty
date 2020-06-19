6f3e778f2558f2182fb0ed13e9842ac70cd05c2d
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2020-06-19 14:00:03 -0400

    partially done benchmarking. trained new model

--- simulation/run_experiment.sh
+++ simulation/run_experiment.sh
@@ -6,44 +6,44 @@ do
 	sleep 15
 done
 
-for i in {1..5}
-do 
-	python3 belief_3D.py pick-n-roll
-	sleep 15
-done
+# for i in {1..5}
+# do 
+# 	python3 belief_3D.py pick-n-roll
+# 	sleep 15
+# done
 
-for i in {1..5}
-do 
-	python3 belief_3D.py bag-sort
-	sleep 15
-done
+# for i in {1..5}
+# do 
+# 	python3 belief_3D.py bag-sort
+# 	sleep 15
+# done
 
-for i in {1..5}
-do 
-	python3 belief_3D.py sbp
-	sleep 15
-done
+# for i in {1..5}
+# do 
+# 	python3 belief_3D.py sbp
+# 	sleep 15
+# done
 
-for i in {1..5}
-do 
-	python3 belief_3D.py optimistic
-	sleep 15
-done
-
-
+# for i in {1..5}
+# do 
+# 	python3 belief_3D.py optimistic
+# 	sleep 15
+# done
 
 
-for i in {1..5}
-do 
-	python3 belief_3D.py mc-dynamic
-	sleep 15
-done
 
-for i in {1..5}
-do 
-	python3 belief_3D.py weighted-dynamic
-	sleep 15
-done
+
+# for i in {1..5}
+# do 
+# 	python3 belief_3D.py mc-dynamic
+# 	sleep 15
+# done
+
+# for i in {1..5}
+# do 
+# 	python3 belief_3D.py weighted-dynamic
+# 	sleep 15
+# done
 
 for i in {1..5}
 do 
