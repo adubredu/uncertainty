652aef4cb8b932c751c3600d652750524535d7fd
Alphonsus Adu-Bredu <alphonsusbq436@gmail.com>
2020-06-19 04:42:11 -0400

    started benchmarking

--- simulation/run_experiment.sh
+++ simulation/run_experiment.sh
@@ -1,55 +1,55 @@
 #! /bin/bash
 
-# for i in {1..5}
-# do 
-# 	python3 belief_3D.py conveyor-belt
-# 	sleep 15
-# done
+for i in {1..5}
+do 
+	python3 belief_3D.py conveyor-belt
+	sleep 15
+done
 
-# for i in {1..5}
-# do 
-# 	python3 belief_3D.py pick-n-roll
-# 	sleep 15
-# done
+for i in {1..5}
+do 
+	python3 belief_3D.py pick-n-roll
+	sleep 15
+done
 
-# for i in {1..5}
-# do 
-# 	python3 belief_3D.py bag-sort
-# 	sleep 15
-# done
+for i in {1..5}
+do 
+	python3 belief_3D.py bag-sort
+	sleep 15
+done
 
-# for i in {1..5}
-# do 
-# 	python3 belief_3D.py sbp
-# 	sleep 15
-# done
+for i in {1..5}
+do 
+	python3 belief_3D.py sbp
+	sleep 15
+done
 
-# for i in {1..5}
-# do 
-# 	python3 belief_3D.py optimistic
-# 	sleep 15
-# done
-
-
+for i in {1..5}
+do 
+	python3 belief_3D.py optimistic
+	sleep 15
+done
 
 
-# for i in {1..5}
-# do 
-# 	python3 belief_3D.py mc-dynamic
-# 	sleep 15
-# done
 
-# for i in {1..5}
-# do 
-# 	python3 belief_3D.py weighted-dynamic
-# 	sleep 15
-# done
 
-# for i in {1..5}
-# do 
-# 	python3 belief_3D.py divergent-dynamic
-# 	sleep 15
-# done
+for i in {1..5}
+do 
+	python3 belief_3D.py mc-dynamic
+	sleep 15
+done
+
+for i in {1..5}
+do 
+	python3 belief_3D.py weighted-dynamic
+	sleep 15
+done
+
+for i in {1..5}
+do 
+	python3 belief_3D.py divergent-dynamic
+	sleep 15
+done
 
 for i in {1..5}
 do 
